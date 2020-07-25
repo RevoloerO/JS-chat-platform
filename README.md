a9a87fa6548cdf941ebd2296f6b34ca9b66e316f
RevoloerO <59490873+RevoloerO@users.noreply.github.com>
2020-07-25 19:52:22 -0400

    initial

---
+++ README.md
@@ -0,0 +1,13 @@
+Owner: Vuong Quyen Mai ()
+Project: Chatty
+Languages: JavaScript, HTML, CSS
+Methods: nodeJS, socket.io, express, moment, nodemon, querystring
+Description: Personal project using JavaScript to create chat platform.
+Run on localhost:
+- open terminal or command prompt
+- install npm, nodeJS
+- redirect to the folder
+- for user: $node server.js
+- for dev:  $npm run dev
+
+Tutorial credit to Traversy Media.
---
+++ server.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socket = require('socket.io');
+const formatMessage = require('./utils/messages')
+const {userJoin, getCurrentUser, userLeave,getRoomUsers} = require('./utils/user')
+
+
+const app = express()
+const server = http.Server(app)
+const io = socket(server)
+
+app.use(express.static(path.join(__dirname,'public')))
+
+const botName = 'Chatty Bot'
+//run client side
+io.on('connection', socket =>{
+  socket.on('joinRoom',({username,room})=>{
+    const user = userJoin(socket.id, username, room)
+
+    socket.join(user.room)
+    //console.log('New User Connection..')
+    socket.emit('message', formatMessage(botName,'Chatty Time'))
+    //broadcast when connect
+    socket.broadcast
+    .to(user.room)
+    .emit(
+      'message',
+      formatMessage(botName,`${user.username} has joined`)
+    )
+    //send users and room info
+    io.to(user.room).emit('roomUsers',{
+      room: user.room,
+      users: getRoomUsers(user.room)
+    })
+  })
+  //listen for chatMessage
+  socket.on('chatMessage', msg=>{
+    const user = getCurrentUser(socket.id)
+    //console.log(msg)
+    io.to(user.room).emit('message',formatMessage(user.username ,msg))
+  })
+  //when user disconnect
+  socket.on('disconnect',()=>{
+    const user = userLeave(socket.id)
+    if(user){
+      io.to(user.room)
+      .emit('message',formatMessage(botName,`${user.username} has left`))
+    }
+
+  })
+})
+const PORT = 3000 || process.env.PORT
+
+server.listen(PORT, ()=>{
+  console.log(`Server running on port ${PORT}`)
+})
